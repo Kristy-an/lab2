0d2e1b18ac51e8ef6a30dc39671965ae14bd3c0d
Hongyan Zhao <hzhao292@wisc.edu>
2023-03-26 16:31:55 -0500

    lab1version

--- requester.py
+++ requester.py
@@ -1,7 +1,8 @@
 import socket
 import argparse
-import sys
 import struct
+import datetime
+import sys
 
 # Packet types
 REQUEST_PACKET = b'R'
@@ -16,94 +17,139 @@ BUFFER_SIZE = 4096
 TRACKER = 'tracker.txt'
 
 
+def print_packet_type(time, response_type, senderip, senderport, response_seq_num, response_length, payload):
+    if response_type == b'D':
+        print("DATA Packet")
+        print("recv time:        ", time)
+        print("sender addr:      ", senderip, ":", senderport)
+        print("sequence:         ", response_seq_num)
+        print("length:           ", response_length)
+        print("payload:          ", payload)
+        print("\n")
+    elif response_type == b'E':
+        print("End Packet")
+        print("recv time:        ", time)
+        print("sender addr:      ", senderip, ":", senderport)
+        print("sequence:         ", response_seq_num)
+        print("length:           ", response_length)
+        print("payload:          ", payload)
+        print("\n")
+
+
+def print_summary(addr, packets_num, data_bytes, duration_ms, sec):
+    print("Summary")
+    print("sender addr:              ", addr)
+    print("Total Data packets:       ", packets_num)
+    print("Total Data bytes:         ", data_bytes)
+    try:
+        print("Average packets/second:   ", packets_num/sec)
+    except ZeroDivisionError:
+        print("Average packets/second:   ", packets_num/(duration_ms/1000))
+    print("Duration of the test:     ", duration_ms, " ms")
+    print("\n")
+
+def cal_milli_diff(time_s, time_end):
+    time_diff_milli = (time_end.second-time_s.second)*1000 + \
+                (time_end.microsecond-time_s.microsecond)/1000
+    return time_diff_milli
+
+def cal_second_diff(time_s, time_end):
+    second_diff = (time_end.minute - time_s.minute) * 60 + \
+                time_end.second - time_s.second
+    return second_diff
+
 # Define a function to parse the tracker file
+# Return a dictionary with ascending id number
 def parse_tracker_file():
     senders = {}
     with open(TRACKER, 'r') as f:
         for line in f:
             fields = line.strip().split()
-            filename, seq_num, hostname, port = fields
-            seq_num = int(seq_num)
+            filename, id, hostname, port = fields
+            id = int(id)
             port = int(port)
-            sender_key = (filename, seq_num)
+            sender_key = (filename, id)
             if sender_key not in senders:
                 senders[sender_key] = []
             senders[sender_key].append((hostname, port))
-    return senders
+            sorted_senders = sorted(senders.items(), key=lambda x: x[0][1])
+            sorted_senders_dict = {key: value for key, value in sorted_senders}
+    return sorted_senders_dict
 
 
-#  Takes in the necessary packet fields, constructs the packet, sends it to the specified
-#  receiver, and waits for a response.
-#  If the response payload is not empty, it is returned.
-def send_request(filename, seq_num, payload, receiver_hostname, receiver_port):
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+if __name__ == '__main__':
+    # Command line argument
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", type=int,
+                        help="port on which the requester waits for packets")
+    parser.add_argument("-o", "--file", type=str,
+                        help="the name of the file that is being requested")
+    args = parser.parse_args()
+    # Process command line argument
+    port = args.port
+    filename = args.file
+    listen_port = args.port
+    listen_ip = '127.0.0.1'
+
+    # create a UDP socket and bind it to the specified address and port
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((listen_ip, port))
+
+    # create a new file for writing the payload data
+    with open(filename, 'wb') as f:
+        senders = parse_tracker_file()
+
+        id = 1
+        seq_num = 0
+        payload = filename
+
         packet_type = b'R'
-        seq_num = socket.htonl(seq_num)
-        length = 0
-        payload = payload.encode()
-        packet = struct.pack('!cII', packet_type, seq_num, length) + payload
-        sock.sendto(packet, (receiver_hostname, receiver_port))
-        data, server = sock.recvfrom(BUFFER_SIZE)
-        response_type, response_seq_num, response_length = struct.unpack('!cII', data[:9])
-        response_payload = data[9:]
-        return response_payload
+        length = len(payload)
+        header = struct.pack(HEADER_FORMAT, packet_type, seq_num, length)
+        packet = header + payload.encode()
 
+        # receive data from each ports
+        while True:
+            key = (filename, id)
+            id += 1
+            try:
+                ip, port = senders[key][0]
+            except KeyError:
+                break
+            sock.sendto(packet, (ip, port))
+            data_packet_count = 0
+            file_len = 0
+            duration = 0
+            recv = True
+            while True:
+                # receive a UDP packet
+                data, server = sock.recvfrom(BUFFER_SIZE)
+                # Record the start time
+                if recv:
+                    start_time = datetime.datetime.now()
+                    recv = False
+                # Record normal packet time
+                time = datetime.datetime.now()
 
-# Uses the parse_tracker_file function to read the tracker file and get a list of sender hostnames
-# and ports for the specified file. Then sends a request packet to each sender in sequence,
-# starting from sequence number 1. If a sender responds with a non-empty payload, it yields the
-# payload to the calling code.
-def request_file(filename):
-    senders = parse_tracker_file()
-    seq_num = 1
-    payload = filename
-    while True:
-        sender_key = (filename, seq_num)
-        if sender_key not in senders:
-            break
-        sender_list = senders[sender_key]
-        for sender in sender_list:
-            response = send_request(filename, seq_num, payload, sender[0], sender[1])
-            if response:
-                yield response
-        seq_num += 1
+                # Parsing package
+                response_type, response_seq_num, response_length = struct.unpack('!cII', data[:9])
+                response_payload = data[9:]
+                file_len += response_length
+                is_not_end = response_type != END_PACKET
 
-
-# Command line argument
-parser = argparse.ArgumentParser()
-parser.add_argument("-p", "--port", type=int,
-                    help="port on which the requester waits for packets")
-parser.add_argument("-o", "--file", type=str,
-                    help="the name of the file that is being requested")
-args = parser.parse_args()
-
-# Process command line argument
-sender_port = args.port
-filename = args.file
-
-# listen_port = args.port
-listen_ip = '127.0.0.1'
-listen_port = 12345
-
-# create a UDP socket and bind it to the specified address and port
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((listen_ip, listen_port))
-
-# receive packets until an empty packet is received
-packets = []
-while True:
-    packet, addr = sock.recvfrom(1024)
-    if not packet:
-        break
-    packets.append(packet)
-
-# write the received data to a file
-with open('received_file.txt', 'wb') as f:
-    data = b''.join(packets)
-    f.write(data)
-
-# print receipt information
-print(f'Received {len(packets)} packets and wrote {len(data)} bytes to received_file.txt')
-
-# close the socket
-sock.close()
+                # Print information & write data to file
+                if is_not_end:
+                    print_packet_type(time, response_type, ip, port, response_seq_num, response_length,
+                                      response_payload.decode()[:4])
+                    f.write(response_payload)
+                    data_packet_count += 1
+                else:
+                    print_packet_type(time, response_type, ip, port, response_seq_num, response_length,
+                                      response_payload.decode()[:4])
+                    end_time = time
+                    duration_ms = cal_milli_diff(start_time, end_time)
+                    duration_sec = cal_second_diff(start_time, end_time)
+                    break
+            print_summary(server, data_packet_count, file_len, duration_ms, duration_sec)
+        f.close()
+        sys.exit()
--- sender.py
+++ sender.py
@@ -1,5 +1,8 @@
 import socket
 import argparse
+import struct
+import datetime
+import time
 import sys
 
 # Packet types
@@ -10,58 +13,94 @@ END_PACKET = b'E'
 # Header format
 HEADER_FORMAT = '!cII'
 
+
+def send_end_packet(seq_num, ip, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    packet_type = END_PACKET
+    length = 0
+    header = struct.pack(HEADER_FORMAT, packet_type, seq_num, length)
+    packet = header
+    sock.sendto(packet, (ip, port))
+    print("END Packet")
+    print("send time:        ", datetime.datetime.now())
+    print("requester addr:   ", ip, ":", port)
+    print("sequence num:     ", seq_num)
+    print("length:           ", length)
+    print("payload:          ", )
+    print("\n")
+
+
 # Command line parameter
 parser = argparse.ArgumentParser()
-parser.add_argument("-p", "--port", type=int,
+parser.add_argument("-p", "--listen", type=int,
                     help="port on which the sender waits for requests")
-parser.add_argument("-g", "--req_port", type=int,
+parser.add_argument("-g", "--send_to", type=int,
                     help="requester port: port on which the requester is waiting")
-parser.add_argument("-r", "--rate",
+parser.add_argument("-r", "--rate", type=int,
                     help="rate: the number of packets to be sent per second")
-parser.add_argument("-q", "--seq_no",
+parser.add_argument("-q", "--seq_no", type=int,
                     help="seq_no: The initial sequence of the packet exchange")
-parser.add_argument("-l", "--length",
+parser.add_argument("-l", "--length", type=int,
                     help="length: the length of the payload(in bytes) in the packets")
 args = parser.parse_args()
 
-# # Limitation of command line parameter
-# if not (2049 < args.port < 65536):
-#     print("Error: Port number should be in the range of 2049 to 65536")
-#     sys.exit(1)
-# if not (2049 < args.req_port < 65536):
-#     print("Error: Requester port number should be in the range of 2049 to 65536")
-#     sys.exit(1)
+# Limitation of command line parameter
+if not (2049 < args.listen < 65536):
+    print("Error: Port number should be in the range of 2049 to 65536")
+    sys.exit(1)
+if not (2049 < args.send_to < 65536):
+    print("Error: Requester port number should be in the range of 2049 to 65536")
+    sys.exit(1)
 
 
 # specify the IP address and port number of the receiver
 requester_ip = '127.0.0.1'
-sender_port = args.port         # port our sender on and listening
-requester_port = args.req_port  # port that our sender should send to
+sendto_port = args.send_to
+listen_this_port = args.listen
 send_rate = args.rate
 payload_length = args.length
-
+seq_num = args.seq_no
+rate = args.rate
 
 # create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# sock.bind((requester_ip, sender_port))
+sock.bind((requester_ip, listen_this_port))
+# print("sender is listening this port: ", listen_this_port, "\n")
 
+# receive packets
+while True:
+    packet, addr = sock.recvfrom(1024)
+    requester_ip = addr[0]
+    udp_header = packet[:9]
+    filename = packet[9:]
 
+    # read the file to send
+    try:
+        with open(filename, 'rb') as f:
+            data = f.read()
+    except FileNotFoundError:
+        send_end_packet()
 
-# read the file to send
-with open('file_to_send.txt', 'rb') as f:
-    data = f.read()
+    # split the data into packets
+    packet_size = args.length
+    packets = [data[i:i + packet_size] for i in range(0, len(data), packet_size)]
 
-# split the data into packets
-packet_size = 1024
-packets = [data[i:i + packet_size] for i in range(0, len(data), packet_size)]
-
-# send each packet to the receiver
-for i, packet in enumerate(packets):
-    sock.sendto(packet, (requester_ip, requester_port))
-    print(f'Sent packet {i + 1}/{len(packets)}')
-
-# send an empty packet to signal end of transmission
-sock.sendto(b'', (requester_ip, requester_port))
-
-# close the socket
-sock.close()
+    # send each packet to the receiver
+    for i, packet in enumerate(packets):
+        packet_type = DATA_PACKET
+        length = len(packet)
+        payload = packet
+        header = struct.pack(HEADER_FORMAT, packet_type, seq_num, length)
+        packet = header + payload
+        sock.sendto(packet, (requester_ip, sendto_port))
+        print("DATA Packet")
+        print("send time:        ", datetime.datetime.now())
+        print("requester addr:   ", requester_ip, ":", sendto_port)
+        print("sequence num:     ", seq_num)
+        print("length:           ", length)
+        print("payload:          ", payload.decode()[:4])
+        print("\n")
+        seq_num = seq_num + length
+        time.sleep(1 / rate)
+    send_end_packet(seq_num, requester_ip, sendto_port)
+    sys.exit()
